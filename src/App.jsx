ba05ae4d8eae08e355647a798a37443aaf86fa79
ptzburn <milan.hokkanen@gmail.com>
2025-05-15 16:12:20 +0300

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Header.jsx'
+import FormPage from './pages/FormPage.jsx'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <FormPage />
     </div>
   )
 }
--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 
 const FormPage = () => {
-  return <div>FormPage</div>
+  return (
+    <>
+      <h1>
+        <span className="max-w-200">
+          Your Journey to Coding Conf 2025 Starts Here!
+        </span>
+      </h1>
+      <p className="text-gray-300 font-mono text-center items-center mt-10">
+        Secure your spot at next year's biggest coding conference.
+      </p>
+      <form />
+    </>
+  )
 }
 export default FormPage
